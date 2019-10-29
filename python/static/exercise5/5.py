fa8a220f6ae7a5e0b05e49b87f49e67ad3055455
racterub <racterub@gmail.com>
2019-10-29 21:44:35 +0800

    add 5.py

--- python/static/exercise5/5.py
+++ python/static/exercise5/5.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @Date    : 2019-10-29 20:34:20
+# @Date    : 2019-10-29 20:05:04
 # @Author  : Racter Liu (racterub)
 # @Mail    : racterub@gmail.com
 # @Link    : https://racterub.io
 # @License : MIT
 
-string = input("Input your string: ")
+string = input(': ')
 
 answer = ''
 tmp = ''
 for i in range(len(string)):
-    if string[i] != ' ':
+    if string[i].isalpha():
         tmp += string[i]
     else:
         answer += tmp[::-1]
         tmp = ''
-        answer += ' '
-
-answer += tmp
-
+        answer += string[i]
+answer += tmp[::-1]
 
 print(answer)
