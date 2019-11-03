7fabd6faff3d3ca12a8054d16d9869f251408ef6
racterub <racterub@gmail.com>
2019-11-03 22:08:17 +0800

    update index.html, fix codes

--- python/static/exercise5/2.py
+++ python/static/exercise5/2.py
@@ -20,20 +20,14 @@ ip = input("Input your IP: ")
 ip_segment = ip.split('.')
 
 #Check if ip has 4 segments
-if (len(ip_segment) != 4):
-    print("Invalid IP")
-    exit()
-
-#Unpack ip segments
-aclass, bclass, cclass, dclass = ip_segment
-#Check a_class and b_class is 140.138
-if (aclass != '140' or bclass != '138'):
-    print("Invalid IP")
-    exit()
-#Check c_class and b_class integer range
-if (0 <= int(cclass) <= 255 and 0 <= int(dclass) <= 255):
-    print("Valid IP")
-    exit()
+if (len(ip_segment) == 4):
+    aclass, bclass, cclass, dclass = ip_segment
+    if (aclass == '140' or bclass == '138'):
+        if (0 <= int(cclass) <= 255 and 0 <= int(dclass) <= 255):
+            print("Valid IP")
+        else:
+            print("Invalid IP")
+    else:
+        print("Invalid IP")
 else:
     print("Invalid IP")
-    exit()
