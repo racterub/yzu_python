a7163ab4f41478bf18951cca4fd85a538788edc9
racterub <racterub@gmail.com>
2019-12-01 20:16:18 +0800

    fixes

--- python/static/exercise6/1.py
+++ python/static/exercise6/1.py
@@ -14,9 +14,10 @@ def validate_triangle():
     c = int(input("c: "))
 
     data_list = [a,b,c]
+    data_list.sort()
 
 
-    if ((max(data_list) ** 2 ) == (data_list[0] ** 2 + data_list[1] ** 2)):
+    if (data_list[2] ** 2 ) == (data_list[0] ** 2 + data_list[1] ** 2):
         return True
     else:
         return False
