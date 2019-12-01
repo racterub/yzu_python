5a26426792cff08c4dceb635536c4b0acf4b4b23
racterub <racterub@gmail.com>
2019-12-01 20:16:43 +0800

    fixes

--- python/static/exercise6/1.py
+++ python/static/exercise6/1.py
@@ -6,20 +6,19 @@
 # @Link    : https://racterub.io
 # @License : MIT
 
-import math
 
 def validate_triangle():
     a = int(input("a: "))
     b = int(input("b: "))
     c = int(input("c: "))
 
-    data_list = [a,b,c]
+    data_list = [a, b, c]
     data_list.sort()
 
-
-    if (data_list[2] ** 2 ) == (data_list[0] ** 2 + data_list[1] ** 2):
+    if (data_list[2] ** 2) == (data_list[0] ** 2 + data_list[1] ** 2):
         return True
     else:
         return False
+
 
 print(validate_triangle())
